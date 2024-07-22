17e4700f443421342cf3ce5b1d2fe969b82e7271
Mark Hodgson <mark.hodgson@breeze-it.com>
2024-07-22 08:52:36 +0100

    vue + flex

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="d-flex">
+
+        <div class="tile tile-1">Tile 1</div>
+        <div class="tile tile-1">Tile 1</div>
+        <div class="tile tile-1">Tile 1</div>
+        <div class="tile tile-1">Tile 1</div>
+        <div class="tile tile-1">Tile 1</div>
+
+    </div>
+
+    <div class="d-flex">
+
+        <div class="tile tile-1">Tile 1</div>
+
+    </div>
+
+    <div class="d-flex">
+
+        <div class="tile tile-1">Tile 1</div>
+
+    </div>
+
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style>
+    .tile-1 {
+        height:calc(30vh - 20px); width:calc(30vh - 20px);
+    }
+</style>
